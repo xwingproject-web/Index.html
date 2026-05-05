407603434642688bbf49606c0c3c0ddd72d26b28
xwingproject-web <xwingproject@gmail.com>
2026-05-05 17:10:30 +0700

    Add admin authentication functions

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,1 +1,24 @@
+const { getDbConnection } = require('../config/database');
+const bcrypt = require('bcrypt');
+
+async function getAdminByUsername(username) {
+  const db = await getDbConnection();
+  return db.get('SELECT * FROM admin_users WHERE username = ?', [username]);
+}
+
+async function verifyAdmin(username, password) {
+  const admin = await getAdminByUsername(username);
+  if (!admin) return false;
+  
+  const match = await bcrypt.compare(password, admin.password_hash);
+  if (match) {
+    return admin;
+  }
+  return false;
+}
+
+module.exports = {
+  getAdminByUsername,
+  verifyAdmin
+};
 
